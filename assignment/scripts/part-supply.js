2ce67ec966534bda5a5c431f1aeefd012e076c3f
Ryan Kamleiter <kamleiterryan@gmail.com>
2023-12-13 14:24:19 -0600

    questions 1-6 working as intended

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -5,25 +5,35 @@ console.log('****** Part Supply *******');
 
 // 1. Create a variable called 'partsNeeded' with a value of the number 40.
 console.log('1. Create partsNeeded:');
+let partsNeeded = 40;
+console.log(partsNeeded);
 
 
 // 2. Create a variable called 'supplyChanges' whose value is an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11.
 console.log('2. Create supplyChanges:');
+let supplyChanges = [3, 5, -6, 0, 7, 11];
+console.log(supplyChanges);
 
 
 // 3. Create a variable called 'secondItem' and assign it the value of the second
 //    item in the 'supplyChanges' array.
 console.log('3. Access the second value of supplyChanges:');
+let secondItem = supplyChanges[1];
+console.log(secondItem);
 
 
 // 4. The last value in the 'supplyChanges' array was added by mistake.
 //    Remove it from the array and store it inside a new variable called 'removedItem'.
 console.log('4. Remove the last value from supplyChanges:');
+let removedItem = supplyChanges.pop();
+console.log(removedItem);
 
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Add the value 25 into supplyChanges.');
+supplyChanges.push(25);
+console.log(supplyChanges);
 
 
 // 6. Create three new variables named 'positives', 'negatives', and
@@ -34,6 +44,22 @@ console.log('5. Add the value 25 into supplyChanges.');
 //      - If the value is a negative number, push it into the 'negatives' array.
 //      - If the value is a zero, push it into the 'zeroes' array.
 console.log('6. Looping through supplyChanges to populate arrays with positive, negative, and zero values:');
+let positives = [];
+let negatives = [];
+let zeroes = [];
+for (let i = 0; i < supplyChanges.length; i++) {
+  if (supplyChanges[i] > 0) {
+    positives.push(supplyChanges[i]);
+  } else if (supplyChanges[i] < 0) {
+    negatives.push(supplyChanges[i]);
+  } else {
+    zeroes.push(supplyChanges[i]);
+  }
+}
+console.log(positives);
+console.log(negatives);
+console.log(zeroes);
+
 
 
 
